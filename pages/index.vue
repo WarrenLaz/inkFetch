dad55f94730b304a72b93f1069b158ca35318697
Warren Lazarraga <79947453+WarrenLaz@users.noreply.github.com>
2025-02-28 01:08:03 -0500

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,7 @@
 <script setup>
 import Canva from '~/components/general/Canva.vue';
 import Colors from '~/components/general/Colors.vue';
-import BrushResize from '~/components/general/BrushResize.vue'
-import { useSliderStore } from '~/store/sliderStore';
 
-const store = useSliderStore();
 </script>
 
 <template>
@@ -15,14 +12,77 @@ const store = useSliderStore();
         <Canva />
         <div>
             <Colors />
-            <BrushResize />
-            {{ store.sliderValue }}
+            <div>
+                <input type="range" min="0" max="100"  class="slider" id="mySlider">
+            </div>
         </div>
+        
     </div>
 </template>
 
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@100..900&display=swap');
+.slider-container {
+      width: 80%;
+      margin: 50px auto;
+      text-align: center;
+    }
+
+    /* Style the slider itself (boxy look) */
+    .slider {
+      -webkit-appearance: none;
+      width: 100%;
+      height: 20px; /* Make it taller for a more boxy feel */
+      background: #ccc; /* Light gray background */
+      border: 2px solid #888; /* Border to make it look boxy */
+      border-radius: 5px; /* Slight rounding of the corners */
+      outline: none;
+      transition: background 0.3s ease;
+    }
+
+    .slider:hover {
+      background: #bbb; /* Slightly darker when hovered */
+    }
+
+    /* Style the thumb (boxy style) */
+    .slider::-webkit-slider-thumb {
+      -webkit-appearance: none;
+      appearance: none;
+      width: 20px; /* Square thumb */
+      height: 20px; /* Square thumb */
+      border: 2px solid #292929; /* Boxy border for the thumb */
+      background: #292929; /* Green color for the thumb */
+      cursor: pointer;
+      border-radius: 0px; /* Square corners */
+    }
+
+    .slider::-moz-range-thumb {
+      width: 20px;
+      height: 20px;
+      border: 2px solid #888;
+      background: #4CAF50;
+      cursor: pointer;
+      border-radius: 0px; /* Square corners */
+    }
+
+    /* Display the value of the slider */
+    .slider-value {
+      margin-top: 10px;
+      font-size: 18px;
+      font-weight: bold;
+      color: #292929;
+    }
+.inner-container {
+    display: flex;
+    align-items: center;
+}
+
+.color-container {
+    width: 75px;
+    height: 75px;
+    margin: 5px;
+    background-color: black;
+}
 
 .inner {
     display: flex;
